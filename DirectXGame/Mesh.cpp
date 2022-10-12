c09b358dedab117fdaa6e9dd34ffc524384dd483
EmersonicsDLSU <emerson_celestial@dlsu.edu.ph>
2022-10-13 06:03:50 +0800

    upd

--- DirectXGame/Mesh.cpp
+++ DirectXGame/Mesh.cpp
@@ -21,6 +21,7 @@ Mesh::Mesh(const wchar_t* full_path) : Resource(full_path)
 	// convert const wchar_t into simple string type
 	std::string inputfile = std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(full_path);
 
+	// parse the object model into a data structure
 	bool res = tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, inputfile.c_str());
 
 	if (!err.empty())
